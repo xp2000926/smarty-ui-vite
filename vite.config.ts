71d28adfcb94fd649507826c7054a3fae6e2de2b
许鹏 <3053949745@qq.com>
2022-08-23 09:30:28 +0800

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,56 @@
+// <reference types="vitest" />
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from '@vitejs/plugin-vue-jsx'
+// import { presetUno, presetAttributify, presetIcons } from "unocss";
+// import Unocss from "unocss/vite";
+import Unocss from "./config/unocss";
+const rollupOptions = {
+  external: ["vue"],
+  output: {
+    globals: {
+      vue: "Vue",
+    },
+  },
+};
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    // 添加JSX插件
+    vueJsx({
+    // options are passed on to @vue/babel-plugin-jsx
+    }),
+     // 添加UnoCSS插件
+    // Unocss({
+    //   presets: [presetUno(), presetAttributify(), presetIcons()],
+    // })
+    Unocss(),
+  ],
+   // 添加库模式配置
+  build: {
+    rollupOptions,
+    minify: 'terser', // boolean | 'terser' | 'esbuild'
+    sourcemap: true, // 输出单独 source文件
+    brotliSize: true,  // 生成压缩大小报告
+    cssCodeSplit: true,   // 追加
+    lib: {
+      entry: "./src/entry.ts",
+      name: "SmartyUI",
+      fileName: "smarty-ui",
+      // 导出模块格式
+      formats: ["esm", "umd","iife"], // 导出模块类型
+    },
+  },
+  test: {
+    // enable jest-like global test APIs
+    globals: true,
+    // simulate DOM with happy-dom
+    // (requires installing happy-dom as a peer dependency)
+    environment: 'happy-dom',
+    // 支持tsx组件，很关键
+    transformMode: {
+      web: [/.[tj]sx$/]
+    }
+  }
+});

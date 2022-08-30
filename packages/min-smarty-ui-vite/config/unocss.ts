1e28586fd43a61f6c8ab7661afb430ef4583fa97
xp2000926 <77706310+xp2000926@users.noreply.github.com>
2022-08-30 08:01:54 +0800

    fix: 修改button字体颜色

--- packages/min-smarty-ui-vite/config/unocss.ts
+++ packages/min-smarty-ui-vite/config/unocss.ts
@@ -13,6 +13,16 @@ const colors = [
   "purple",
   "pink",
 ];
+const icon = [
+  "search",
+  "edit",
+  "check",
+  "message",
+  "star-off",
+  "delete",
+  "add",
+  "share",
+];
 const safelist = [
   ...colors.map((v) => `bg-${v}-100`),
   ...colors.map((v) => `bg-${v}-400`),
@@ -25,29 +35,18 @@ const safelist = [
   ...colors.map((v) => `border-${v}-500`),
   ...colors.map((v) => `text-${v}-500`),
   ...colors.map((v) => `hover:text-${v}-500`),
-
   ...Array.from({ length: 8 }, (_, i) => `px-${i + 1}`),
   ...Array.from({ length: 8 }, (_, i) => `py-${i + 1}`),
   ...["xs", "sm", "base", "lg", "xl", "2xl", "3xl"].map((v) => `text-${v}`),
   ...["rounded-full", "rounded-lg"],
-  ...[
-    "search",
-    "edit",
-    "check",
-    "message",
-    "star-off",
-    "delete",
-    "add",
-    "share",
-  ].map((v) => `i-ic-baseline-${v}`),
+  ...icon.map((v) => `i-ic-baseline-${v}`),
 ];
-
 export default () =>
   Unocss({
     safelist,
     presets: [
-      presetUno(), 
-      presetAttributify(), 
-      presetIcons()// 添加图标预设
+      presetUno(),
+      presetAttributify(),
+      presetIcons(), // 添加图标预设
     ],
   });

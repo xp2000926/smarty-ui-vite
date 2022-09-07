e2520fad87447c155e0d9127d964ed21d11775b2
xp2000926 <77706310+xp2000926@users.noreply.github.com>
2022-09-07 09:25:05 +0800

    ci: day13

--- packages/create-min-smarty-app/lib/utils/clone.js
+++
@@ -1,9 +0,0 @@
-import { promisify } from "util";
-import download from "download-git-repo";
-import ora from "ora";
-export default async (repo, desc) => {
-  const process = ora(`下载.....${repo}`);
-  process.start();
-  await promisify(download)(repo, desc);
-  process.succeed();
-};
